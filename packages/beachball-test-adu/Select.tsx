d1c28fa1e8bf51ce56a27b695163bb863c97d6fb
Aditya Kavale <Aditya.Kavale@bentley.com>
2025-02-04 13:46:12 +0530

    minor changes

--- packages/beachball-test-adu/Select.tsx
+++ packages/beachball-test-adu/Select.tsx
@@ -1,7 +1,14 @@
-function CustomSelect(props: any) {  
-    return <div>
-        prop1: {props.prop1}
-    </div>;  
- }
-  
- export default CustomSelect;
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Adu21, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+const CustomSelect = ({ prop1 = "Hello" }) => {
+  return (
+    <div>
+      <p>prop1: {prop1}</p>
+    </div>
+  );
+};
+
+export default CustomSelect;
