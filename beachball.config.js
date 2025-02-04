f5a90852901ebe4d4e52b152ce88d1a6407ba808
Aditya Kavale <Aditya.Kavale@bentley.com>
2025-02-04 12:34:27 +0530

    minor changes

--- beachball.config.js
+++ beachball.config.js
@@ -8,8 +8,8 @@ module.exports = {
   access: "public",
   tag: "latest",
   scope: ["packages/beachball-test-adu"],
-  package: "@bentley/drawing-production",
-  registry: "https://pkgs.dev.azure.com/bentleycs/_packaging/Packages/npm/registry/",
+  package: "@adu21/beachball-test-adu",
+  registry: "http://localhost:4873/",
   ignorePatterns: [
     "eslint.config.js",
     "prettier-config.js",
@@ -27,33 +27,12 @@ module.exports = {
   changelog: {
     customRenderers: {
       renderEntry: (entry) => {
-        const commitLink = `https://github.com/iTwin/drawing-production/commit/${entry.commit}`;
+        const commitLink = `https://github.com/dp-test-org-1/beachball-testing/commit/${entry.commit}`;
         return `- ${entry.comment} ([commit](${commitLink}))`;
       }
     }
   },
-  changeFilePrompt: {
-    changePrompt: (defaultPrompt, pkg) => {
-      return [
-        defaultPrompt.changeType,
-        defaultPrompt.description,
-        {
-          type: "select",
-          name: "isStudioVersionUpgrade",
-          message: "Is this a studio version upgrade?",
-          choices: [
-            { title: "No", value: "no" },
-            { title: "Yes", value: "yes" }
-          ]
-        },
-        {
-          type: (prev) => (prev === "yes" ? "text" : null),
-          name: "targetStudioVersion",
-          message: "Which version of Studio was used to test these changes?(e.g. 1.0.84)"
-        }
-      ];
-    }
-  },
+
   verbose: true,
   generateChangelog: true,
   verbose: true
