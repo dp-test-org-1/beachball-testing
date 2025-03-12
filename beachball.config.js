0bcd08825655c0639b7fc57aac70d01eac7b2f1b
Aditya Kavale <Aditya.Kavale@bentley.com>
2025-03-12 13:52:43 +0530

    Update beachball.config.js with new package information

--- beachball.config.js
+++ beachball.config.js
@@ -8,6 +8,7 @@ module.exports = {
   access: "public",
   tag: "latest",
   scope: ["packages/beachball-test-adu", "packages/package2"],
+  package: "@adu21/beachball-test-adu",
   registry: "http://localhost:4873/",
   ignorePatterns: [
     "eslint.config.js",
