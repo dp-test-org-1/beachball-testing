bb5e63cb132375317a72b489dc7ad0f42730681e
Aditya Kavale <Aditya.Kavale@bentley.com>
2025-12-16 15:51:45 +0530

    chore: update package.json for beachball-test-adu

--- packages/beachball-test-adu/Select.tsx
+++ packages/beachball-test-adu/Select.tsx
@@ -3,10 +3,13 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
+import CustomComponent from "@adu21/beachball-test-adu-2/CustomComponent";
+
 const CustomSelect = ({ prop1: prop2 = "Hello" }) => {
   return (
     <div>
       <p>prop5: {prop2}</p>
+      <CustomComponent prop1={prop2} />
     </div>
   );
 };
