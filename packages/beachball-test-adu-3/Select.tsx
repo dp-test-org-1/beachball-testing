d4938a935bc65807dafffb0f1b9feee285017d7a
AdityaAtBentley <Aditya.Kavale@bentley.com>
2026-04-14 13:56:03 +0530

    fix: correct prop destructuring in CustomSelect2 for accurate rendering

--- packages/beachball-test-adu-3/Select.tsx
+++ packages/beachball-test-adu-3/Select.tsx
@@ -5,11 +5,11 @@
 
 import CustomComponent from "@adu21/beachball-test-adu-2/CustomComponent";
 
-const CustomSelect2 = ({ prop1: prop2 = "Hello" }) => {
+const CustomSelect2 = ({ prop1 = "Hello" }) => {
   return (
     <div>
-      <p>prop5: {prop2}</p>
-      <CustomComponent prop1={prop2} />
+      <p>prop5: {prop1}</p>
+      <CustomComponent prop1={prop1} />
     </div>
   );
 };
